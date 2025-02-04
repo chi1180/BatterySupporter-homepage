452340afc74adc707dec289bdc2b9517accc078a
chi1180 <cwchi1@outlook.jp>
2025-02-04 09:35:32 +0900

    verfication added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "BatterySupporter home page",
   description: "A home page of BatterySupporter",
+  verification: { google: "dVRTS_9lJ9SL6_YPh2Il8afiUi0v_3EbbHesLSBdDeo" },
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-    <body>
-    {children}
-    </body>
+      <body>{children}</body>
     </html>
   );
 }
